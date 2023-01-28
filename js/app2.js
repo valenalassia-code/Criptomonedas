df2921c0bf771866491f405cad4b35f86bfc00a7
Valentin Alassia <valentinalassia1@gmail.com>
2023-01-28 20:42:35 +0100

    Agregamos async a la funcion consultarCriptomoned

--- js/app2.js
+++ js/app2.js
@@ -22,14 +22,16 @@ document.addEventListener('DOMContentLoaded', () =>{
     monedaSelect.addEventListener('change', leerValor)
 });
 
-function consultarCriptomonedas(){
+async function consultarCriptomonedas(){
     const url = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD`;
-
-    fetch(url)
-        .then(respuesta => respuesta.json())
-        .then(resultado =>obtenerCriptomonedas(resultado.Data))
-        .then(criptomonedas =>selectCriptomonedas(criptomonedas))
-    
+        try {
+            const respuesta = await fetch(url)
+            const resultado = await respuesta.json()
+            const criptomonedas = await obtenerCriptomonedas(resultado.Data)
+            selectCriptomonedas(criptomonedas)
+        } catch (error) {
+            console.log(error)
+        }
 }
 
 function selectCriptomonedas(criptomonedas){
